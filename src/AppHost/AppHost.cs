a699699e2b56d530bdb5ea9f5311326f015233b9
Peter Nylander <peter.nylander@gmail.com>
2026-03-02 21:09:11 +0100

    Provision SQL Server container and wire up database config
    Automate provisioning of a SQL Server container with a persistent data volume and a "MeetupPlanner" database. Update the API project to reference and wait for the new database, and set its connection string dynamically via environment variable. Move builder.Build().Run() to the end of the file. Comment out legacy Keycloak and frontend environment variable lines for reference.

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -1,7 +1,15 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Add a SQL Server container
+var sqlServer = builder.AddSqlServer("sqlserver", port: 1433)
+    .WithDataVolume("meetuppplanner")
+    .WithImage("mssql/server", "latest")
+    .WithLifetime(ContainerLifetime.Persistent);
+
+var database = sqlServer.AddDatabase("db", "MeetupPlanner");
+
 // Add a database connection string
-var db = builder.AddConnectionString("MeetupPlanner");
+var dbConnectionString = builder.AddConnectionString("MeetupPlanner");
 
 // Keycloak
 var keycloakdUsername = builder.AddParameter("username");
@@ -19,12 +27,15 @@ keycloakPassword.WithParentRelationship(keycloak);
 
 // Projects
 var api = builder.AddProject<Projects.MeetupPlanner_Api>("api")
-    .WaitFor(db)
-    .WithReference(db)
-    .WithReference(keycloak);
-
-var adminApp = builder.AddProject<Projects.MeetupPlanner_Admin>("meetupplanner-admin")
-    .WithReference(api);
+    .WaitFor(dbConnectionString)
+    .WithReference(dbConnectionString)
+    .WithReference(keycloak)
+    .WithReference(database)
+    .WaitFor(database)
+    .WithEnvironment(async context =>
+    {
+        context.EnvironmentVariables["MeetupPlanner:ConnectionString"] = await database.Resource.ConnectionStringExpression.GetValueAsync(CancellationToken.None) ?? string.Empty;
+    });
 
 var web = builder.AddViteApp("web", "../Web")
     .WaitFor(api)
@@ -38,13 +49,20 @@ var bff = builder.AddProject<Projects.MeetupPlanner_Bff>("bff")
     .WithReference(keycloak).WaitFor(keycloak)
     .WithReference(adminWeb)
     .WithEnvironment("OpenIDConnectSettings__ClientSecret", openIDConnectSettingsClientSecret)
-    //.WithEnvironment("Authentication__KeyCloak__Authority", () => keycloak.GetEndpoint("http")!.Url.TrimEnd('/') + "/realms/meetupplanner")
-    //.WithEnvironment("Authentication__KeyCloak__RevocationEndpoint", () => keycloak.GetEndpoint("http")!.Url.TrimEnd('/') + "/realms/meetupplanner/protocol/openid-connect/revoke")
-    //.WithEnvironment("Frontend__DevServer", () => adminWeb.GetEndpoint("http")!.Url)
     .WithExternalHttpEndpoints();
 
 adminWeb.WithReference(bff);
 
+builder.Build().Run();
+
+
+
+
+
+
+//.WithEnvironment("Authentication__KeyCloak__Authority", () => keycloak.GetEndpoint("http")!.Url.TrimEnd('/') + "/realms/meetupplanner")
+//.WithEnvironment("Authentication__KeyCloak__RevocationEndpoint", () => keycloak.GetEndpoint("http")!.Url.TrimEnd('/') + "/realms/meetupplanner/protocol/openid-connect/revoke")
+//.WithEnvironment("Frontend__DevServer", () => adminWeb.GetEndpoint("http")!.Url)
 //api.WithEnvironment("Cors__AllowedOrigins", () => bff.GetEndpoint("http")!.Url.TrimEnd('/'));
 //api.WithEnvironment("KeyCloak__Authority", () => keycloak.GetEndpoint("http")!.Url.TrimEnd('/') + "/realms/meetupplanner");
 //api.WithEnvironment("KeyCloak__ClientId", "admin-bff");
@@ -72,6 +90,3 @@ adminWeb.WithReference(bff);
 //    })
 //    .WithExternalHttpEndpoints()
 //    .PublishWithStaticFiles(web);
-
-
-builder.Build().Run();
